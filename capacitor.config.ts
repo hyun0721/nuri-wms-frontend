2e55eb2e45439ce3a1bff2dfd0c3bb30635da0ac
donghyun <hyunee721@gmail.com>
2023-07-09 21:40:00 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nuri_wms',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
