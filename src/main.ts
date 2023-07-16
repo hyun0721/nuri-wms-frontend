7f265e80427fde1cf7582762c3319b536e359444
donghyun <hyunee721@gmail.com>
2023-07-16 23:47:56 +0900

    [master] Fontawesome 추가

--- src/main.ts
+++ src/main.ts
@@ -23,15 +23,30 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* BootStarp (Vue3) */
 import BootstrapVue3 from 'bootstrap-vue-3';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
+/* The fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
+import { faTruck, faTruckRampBox, faBoxOpen } from '@fortawesome/free-solid-svg-icons';
+// import { faLightbulb, faCircleCheck  } from '@fortawesome/free-regular-svg-icons'
+// import { faBlogger } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faTruck);
+library.add(faTruckRampBox);
+library.add(faBoxOpen);
 
 const app = createApp(App)
   .use(IonicVue)
   .use(router)
-  .use(BootstrapVue3);
+  .use(BootstrapVue3)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .component('font-awesome-layers', FontAwesomeLayers)
+  .component('font-awesome-layer-text', FontAwesomeLayersText)
+  ;
   
 router.isReady().then(() => {
   app.mount('#app');
